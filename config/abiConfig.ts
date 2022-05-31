c042c1e5e1605b65cca9df5406a3d36f1734c624
Soohan-Park <725psh@gmail.com>
2022-05-31 10:48:38 +0900

    Develop common files

---
+++ config/abiConfig.ts
@@ -0,0 +1,33 @@
+import { AbiItem } from "caver-js";
+
+interface ABIConfig {
+  abi: AbiItem[];
+}
+
+const abiConfig: ABIConfig = {
+  abi: [
+    {
+      inputs: [{ internalType: "bytes4", name: "interfaceId", type: "bytes4" }],
+      name: "supportsInterface",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+      name: "ownerOf",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalSupply",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+  ],
+};
+
+export default abiConfig;
